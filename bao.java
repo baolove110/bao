e175717dd9d486833caa0dad28f7dc8dbec0c5b5
bht <1156213478@qq.com>
2016-01-09 21:59:59 +0800

    2016.1.9

--- bao.java
+++ bao.java
@@ -2,5 +2,6 @@ public class bao{
 	public static void main (String[]args){
 		system.out.println("baoziyeye");
                 system.out.println("gedit");
+                system.out.println("2016.1.9");
 	}
 }
